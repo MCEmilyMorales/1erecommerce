ff67eabd6f1acb883cd117a7120ecb53a4ab03a5
Emily Morales <15ruth05@gmail.com>
2024-07-04 23:35:58 -0300

    define DB_HOST y DB_PORT

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,11 +6,11 @@ dotenvConfig({
   path: '.env',
 });
 
-
 const config = {
   type: 'postgres',
   //host: process.env.DB_HOST,
-  host: 'postgresdb',
+  //host: 'postgresdb',
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
@@ -18,11 +18,10 @@ const config = {
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   logging: false,
-  synchronize: false, // ayuda a sincronizar con la base de datos
-
+  synchronize: true, // ayuda a sincronizar con la base de datos
   dropSchema: true,
 };
 console.log('DB_HOST:', process.env.DB_HOST);
 
-export const typeOrmConfig = registerAs('typeorm', () => config); // registra en la configuracion de nest este objeto de'config' 
-export const connectionSource = new DataSource(config as DataSourceOptions); // configura de parte de typeorm este objeto de config y le da el nombre de DataSourceOptions para toda la app 
+export const typeOrmConfig = registerAs('typeorm', () => config); // registra en la configuracion de nest este objeto de'config'
+export const connectionSource = new DataSource(config as DataSourceOptions); // configura de parte de typeorm este objeto de config y le da el nombre de DataSourceOptions para toda la app
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { logger } from './middlewares/logger.middleware';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,6 @@ async function bootstrap() {
 
   app.use(logger);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //gracias a whitelist, solo me va a tomar la informacion que tengo validadar- me va a permitir ahorrar escribir lineas de codigo para crear un objeto o nueva instacia que solo me  permita tomar la informacion que quiero osea que por body-
-  await app.listen(3000);// escucha en el puerto = 3000
+  await app.listen(PORT); // escucha en el puerto = 3000
 }
 bootstrap();
